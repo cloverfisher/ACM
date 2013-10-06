11de520f68aca250d2c4c8f5208b2d2ceaf8c8d6
ShellYu <yuysyu@gmail.com>
2013-10-06 14:38:32 -0400

    This is accepted!!

--- ECRC2012A/ECRC2012A/main.c
+++ ECRC2012A/ECRC2012A/main.c
@@ -8,7 +8,7 @@
 //  The 2012 East Central Regional Contest Problem A: Babs' Box Boutique
 
 #include <stdio.h>
-
+#include <string.h>
 
 int x[10];
 int y[10];
@@ -27,11 +27,11 @@ int recursion (int num);
 int bubble(int Xs[], int Ys[],int num);
 int longestdecrease(int Ys[],int num);
 
-int main(int argc, const char * argv[])
+int main()
 {
 
     // insert code here...
-    printf("Hello, World!\n");
+  //  printf("Hello, World!\n");
     input();
     return 0;
 }
@@ -39,9 +39,9 @@ int main(int argc, const char * argv[])
 
 int input()
 {
-    int num;
-    int tempnum;
-    int i;
+    int num = 0 ;
+    int tempnum = 0;
+    int i = 0 ;
     int xyz[3];
     int flag = 3;
     int casetag=1;
@@ -51,7 +51,7 @@ int input()
     {
         scanf("%d",&tempnum);
         if(tempnum == 0)
-            break;
+            return 1;
         if(num == 0)
         {
             num = tempnum;
@@ -74,7 +74,7 @@ int input()
 
                     if(num == 0)
                     {
-                        recursion(celllength);
+                        recursion(celllength-1);
                         printf("Case %d: %d\n",casetag,max);
                         casetag++;
                         max = 0;
@@ -92,16 +92,22 @@ int input()
 
 int recursion (int num)
 {
-    int i;
+    int i = 0;
+    int xt[10];
+    int yt[10];
 
         for (i = 0;i<3 ; i++)
         {
             if (i==0) {
-                x[num] = cell[num][1]>cell[num][2] ? cell[num][1]:cell[num][2];
+                x[num] = (cell[num][1]>cell[num][2]) ? cell[num][1]:cell[num][2];
                 y[num] = (x[num]==cell[num][1]) ? cell[num][2]: cell[num][1];
                 if(num == 0)
                 {
-                    bubble(x,y,celllength);
+                    memcpy(xt,x,sizeof(x));
+                    memcpy(yt,y,sizeof(y));
+                    bubble(xt,yt,celllength);
+
+            //        bubble(x,y,celllength);
                 }
                 else
                 {
@@ -109,11 +115,15 @@ int recursion (int num)
                 }
             }
             else if (i==1) {
-                x[num] = cell[num][0]>cell[num][2] ? cell[num][0]:cell[num][2];
+                x[num] = (cell[num][0]>cell[num][2]) ? cell[num][0]:cell[num][2];
                 y[num] = (x[num]==cell[num][0]) ? cell[num][2]: cell[num][0];
                 if(num == 0)
                 {
-                    bubble(x,y,celllength);
+                    memcpy(xt,x,sizeof(x));
+                    memcpy(yt,y,sizeof(y));
+                    bubble(xt,yt,celllength);
+
+           //         bubble(x,y,celllength);
                 }
                 else
                 {
@@ -121,11 +131,15 @@ int recursion (int num)
                 }
             }
             else if (i==2) {
-                x[num] = cell[num][1]>cell[num][0] ? cell[num][1]:cell[num][0];
+                x[num] = (cell[num][1]>cell[num][0]) ? cell[num][1]:cell[num][0];
                 y[num] = (x[num]==cell[num][1]) ? cell[num][0]: cell[num][1];
                 if(num == 0)
                 {
-                    bubble(x,y,celllength);
+                    memcpy(xt,x,sizeof(x));
+                    memcpy(yt,y,sizeof(y));
+                    bubble(xt,yt,celllength);
+
+           //         bubble(x,y,celllength);
                 }
                 else
                 {
@@ -143,6 +157,11 @@ int bubble(int Xs[], int Ys[],int num)
     int i =0;
     int j = 0;
     int tempx,tempy;
+//    printf("%d %d\n%d %d\n%d %d\n%d %d\n ----------------\n",Xs[0],Ys[0],Xs[1],Ys[1],Xs[2],Ys[2],Xs[3],Ys[3]);
+//    int Xs[10]; //= Xs;
+//    int Ys[10];
+//    memcpy(Xs,Xt,num);
+//    memcpy(Ys,Yt,num);
     for(i = 0 ; i < num-1 ; i++)
         for (j = i+1; j < num ; j++)
         {
@@ -155,14 +174,24 @@ int bubble(int Xs[], int Ys[],int num)
                 Ys[i] = Ys[j];
                 Ys[j] = tempy;
             }
+            else if(Xs[i] == Xs[j])
+            {
+                if(Ys[i] < Ys[j])
+                {
+                    tempy = Ys[i];
+                    Ys[i] = Ys[j];
+                    Ys[j] = tempy;
+                }
+            }
         }
+  //  printf("%d %d\n%d %d\n%d %d\n%d %d\n ",Xs[0],Ys[0],Xs[1],Ys[1],Xs[2],Ys[2],Xs[3],Ys[3]);
     longestdecrease(Ys,num);
     return 1;
 }
 
 int longestdecrease(int Ys[],int num)
 {
-    int i,j;
+    int i = 0,j =0;
     int lis[num];
     for(i=0;i<num;i++)
     {
@@ -178,7 +207,15 @@ int longestdecrease(int Ys[],int num)
     for(i=0;i<num;i++)
     {
         if (max<lis[i])
+        {
             max = lis[i];
+     //       printf("%d,%d,%d,%d,%d,%d,%d,%d,%d\n",Ys[0],Ys[1],Ys[2],Ys[3],Ys[4],Ys[5],Ys[6],Ys[7],Ys[8]);
+        }
     }
+//    if(Ys[1]==9)
+//    printf("%d,%d,%d,%d,%d,%d,%d,%d,%d\n",Ys[0],Ys[1],Ys[2],Ys[3],Ys[4],Ys[5],Ys[6],Ys[7],Ys[8]);
+
+//    printf("%d,%d,%d\n",Ys[0],Ys[1],Ys[2]);
+ //   printf("Max %d\n",max);
     return 1;
 }
