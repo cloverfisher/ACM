9267b93d8024c64f909c7c154c14e36ad52daa23
ShellYu <yuysyu@gmail.com>
2013-10-28 21:23:25 -0400

    add some readme in 2011 ECNA

--- ECNA2011/a/agency.c
+++ ECNA2011/a/agency.c
@@ -1,14 +1,17 @@
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 	int N;
-	char sourcebits[1000];
-	char destinybits[1000];
-	int tax[1000];
-
+	char sourcebits[1005];
+	char destinybits[1005];
+	long tax[1005];
+int step =0;
+char tempbits[1005];
 int sort()
 {
 	int i,j;
-	int temptax;
+	long temptax;
 	char tempbit;
 	for(i=0;i<N;i++)
 	{
@@ -18,7 +21,7 @@ int sort()
 			{
 				temptax = tax[j];
 				tax[j] = tax[i];
-				tax[i] = tamptax;
+				tax[i] = temptax;
 				tempbit = sourcebits[j];
 				sourcebits[j] = sourcebits[i];
 				sourcebits[i] = tempbit;
@@ -28,35 +31,152 @@ int sort()
 			}
 		}
 	}
+	return 0;
+}
+
+long sum(char bits[1005])
+{
+	long num=0;
+	int i=0;
+	for(i=0;i<N;i++)
+		num=num + (bits[i]-'0')*tax[i];
+	return num;
+}
+
+
+long function(int num,char bits[1005])
+{
+	char newbits[1005];
+	long num1,num2;
+//	printf("%s\n",bits);
+	memset(newbits,0,sizeof(newbits));
+	if(num == N-1)
+	{
+		if(sourcebits[num]=='0' && destinybits[num]=='0')
+			return 0;
+		if(sourcebits[num]=='1' && destinybits[num]=='1')
+			return 0;
+		else
+		{
+			strcpy(newbits,bits);
+			newbits[num] = destinybits[num];
+			step++;
+	//		printf("sum : %d\n",sum(bits));
+			return sum(newbits);
+		}
+	}
+
+	if(sourcebits[num]=='0' && destinybits[num]=='0')
+	{
+	//	memset(tempbits,0,sizeof(tempbits));
+	//	strcpy(tempbits,bits);
+	//	num1 = function(num+1,tempbits);
+		return function(num+1,bits);
+	}
+	else if(sourcebits[num]=='1' && destinybits[num]=='0')
+	{
+	//	printf("1-->0\n");
+	//	memset(newbits,0,sizeof(newbits));
+		strcpy(newbits,bits);
+	//	printf("bits : %s\n",bits);
+		//strcpy(bits,newbits);
+		newbits[num]='0';
+		num2 = sum(newbits);
+	//	printf("newbits :%s\n",newbits);
+	//	return 0;	
+	
+		num1 = function(num+1,newbits);
+	step++;
+	//		printf("a num1:%d,sum:%d\n",num1,sum(newbits));
+		return num2+num1;
+	}
+	else if(sourcebits[num]=='0' && destinybits[num]=='1')
+	{
+	//			memset(newbits,0,sizeof(newbits));
+		strncpy(newbits,bits,num);
+		strcat(newbits,&destinybits[num]);//the right side is bits,the right side is destiny
+		//newbits[num]='1';
+	//	printf("%d: %s\n",num,newbits);
+	//	memset(tempbits,0,sizeof(tempbits));
+	//	strcpy(tempbits,bits);
+	//	num1 = function(num+1,tempbits);
+		num2 = function(num+1,bits);
+		step++;
+		return sum(newbits)+ num2;
+	}
+	else if(sourcebits[num]=='1' && destinybits[num]=='1')
+	{
+	//	memset(tempbits,0,sizeof(tempbits));
+	//	strcpy(tempbits,bits);
+		num1 = function(num+1,bits);
+
+	//	strncpy(newbits,bits,num);
+	//	strcat(newbits,destinybits[num]);//the right side is bits,the right side is destiny
+		strcpy(newbits,bits);	
+		newbits[num]='1';
+		num2 = sum(newbits);//+function(num+1,newbits);
+	//	printf("num2:%d\n",num2);
+		memset(newbits,0,sizeof(newbits));
+		strncpy(newbits,bits,num);
+		strcat(newbits,&destinybits[num]);//the right side is bits,the right side is destiny
+		newbits[num]='1';
+		num2 = num2 + sum(newbits);
+	//	printf("num2:%d\n",num2);
+		//		printf("num1:%d\n",num1);
+		if(num2>=tax[num]*(step+1))
+		{
+			return num1;
+		}
+		else
+		{
+			step+=2;
+			return num1-tax[num]*(step-1)+num2;
+		}
+	//	return (num1>num2)?num2:num1;
+	}
+
+	return 0;
+
 }
 
 int main()
 {
 	int i,j,k;
 	
-
+	j=1;
+	freopen("read.in","r",stdin);
 	scanf("%d",&N);
 	getchar();
 	while(N!=0)
 	{
+		step =0;
 		 //gets(sourcebits);
-		 //gets(destinybits);
+		 //gets(destinybits); 
+		memset(sourcebits,0,sizeof(sourcebits));
+		memset(destinybits,0,sizeof(destinybits));
+	//	memset(tax,)
 		scanf("%s",sourcebits);
 		scanf("%s",destinybits);
-	
+
 		for(i=0;i<N;i++)
 		{
-			scanf("%d",tax[i]);
+			scanf("%ld",&tax[i]);
 		}
-	
-	
-		printf("%s",sourcebits);
-		printf("%s",destinybits);
-	
+				sort();
+		printf("Case %d: %ld\n",j,function(0,sourcebits));
+		j++;
+	//	printf("%d\n",function(0,sourcebits));
+	//	strncpy(sourcebits,destinybits,5);
+	//	strcat(sourcebits,&destinybits[1]);
+//strcpy(sourcebits,destinybits);
+//		printf("%s",sourcebits);
+	//	printf("%s",destinybits);
+		scanf("%d",&N);
+		getchar();
 	
 		}
 
-	}
+	
 //	for(i=0;i<N;i++)
 //	for(i=0;i<N;i++)
 
--- ECNA2011/a/readme.md
+++ ECNA2011/a/readme.md
@@ -3,3 +3,46 @@ The Angency
 a test from [UVAlive 5780](https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3791)
 --------------------------------------------------------------------
 
+
+Following in the footsteps of a number of flight searching startups you want to create the first interplanetary travel website. Your first problem is to quickly find the cheapest way to travel between two planets. You have an advantage over your competitors because you have realized that all the planets and the flights between them have a special structure. Each planet is represented by a string of N bits and there is a flight between two planets if their N-bit strings differ in exactly one position.
+
+The cost of a flight is the cost of landing on the destination planet. If the ith character in a planet's string is a 1 then the ith tax must be paid to land. The cost of landing on a planet is the sum of the applicable taxes.
+
+Given the starting planet, ending planet, and cost of the ith tax compute the cheapest set of flights to get from the starting planet to the ending planet.
+
+Input
+------
+Input for each test case will consist of two lines. The first line will have N (1$ \le$N$ \le$1, 000), the number of bits representing a planet; S, a string of N zeroes and ones representing the starting planet; and E, a string representing the ending planet in the same format. The second line will contain N integers the ith of which is the cost of the ith tax. All costs will be between 1 and 1,000,000. The input will be terminated by a line with a single `0'.
+
+Output
+------
+For each test case output one number, the minimum cost to get from the starting planet to the ending planet, using the format given below.
+
+
+Sample Input
+------------
+3 110 011
+3 1 2
+5 00000 11111
+1 2 3 4 5
+4 1111 1000
+100 1 1 1
+30 000000000000000000000000000000 111111111111111111111111111111
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+0
+
+Sample Output
+-------------
+Case 1: 4
+Case 2: 35
+Case 3: 106
+Case 4: 4960
+
+
+Standard Input
+--------------
+[input](http://acm.ashland.edu/2011/Problem-Set/Data/A.in)
+
+Standard Output
+----------------
+[output](http://acm.ashland.edu/2011/Problem-Set/Data/A.out) 
